287e42c8e366ec4479f93788705b9da3e91b903b
tim <timhdm@ya.ru>
2024-05-21 16:18:54 +0300

    non blocking code

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,20 @@
 #include <CheapStepper.h>
 
 CheapStepper stepper(2, 3, 4, 5);
+bool moveClockwise = true;
+unsigned long moveStartTime = 0;
 
 void setup() {
   Serial.begin(115200);
-  stepper.setRpm(10);
-  Serial.println();
-  Serial.print(stepper.getRpm());
-  Serial.print(" rpm = delay of ");
-  Serial.print(stepper.getDelay());
-  Serial.println(" microseconds between steps");
-
-  // stepper.setTotalSteps(4076);
-  /* you can uncomment the above line if you think your motor
-   * is geared 63.68395:1 (measured) rather than 64:1 (advertised)
-   * which would make the total steps 4076 (rather than default 4096)
-   * for more info see: http://forum.arduino.cc/index.php?topic=71964.15
-   */
+  stepper.setRpm(24);
 }
 
 void loop() {
-  static uint16_t counter = 0;
-  counter++;
-  stepper.moveDegreesCW(360 * counter);
-  Serial.println("[DEGREES] Step position: " + String(stepper.getStep()) + " / 4096");
-  delay(1000);
-}
+  stepper.run();
 
+  if (stepper.getStepsLeft() == 0) {
+    Serial.println("Turn " + String(millis() - moveStartTime) + " ms.");
+    stepper.newMoveDegreesCW(360);
+    moveStartTime = millis();
+  }
+}
