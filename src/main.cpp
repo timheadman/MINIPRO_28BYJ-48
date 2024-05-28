f1152e962124eed54bdda50a79a014d271e82661
tim <timhdm@ya.ru>
2024-05-28 11:55:42 +0300

    add dht

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <CheapStepper.h>
 
+#include "DHTesp.h"
+
 #define PIN_MIN_POSITION_SWITCH 2  // INT0
 #define PIN_MAX_POSITION_SWITCH 3  // INT1
 #define PIN_STEPPER_1 4
@@ -8,19 +10,20 @@
 #define PIN_STEPPER_3 6
 #define PIN_STEPPER_4 7
 #define PIN_MOTOR_POWER 8
+#define PIN_DHT22 9
 #define PIN_LED 13
 
 #define MAX_TURN_LIMIT 5
-#define MAX_CW_RPM 24
-#define MAX_CCW_RPM 12
+#define MAX_RPM 12
 
 CheapStepper stepper(PIN_STEPPER_1, PIN_STEPPER_2, PIN_STEPPER_3,
                      PIN_STEPPER_4);
+DHTesp dht;
+uint32_t timerMillis = 0;
 volatile int32_t currentTurn = 0;
-volatile int32_t maxTurn = 0;
 
-void turnToPush(uint16_t turns);
-void turnToPull(uint16_t turns);
+void pushActuator();
+void pullActuator();
 void powerOnStepper();
 void powerOffStepper();
 void gotoZero();
@@ -29,26 +32,33 @@ void interruptMaxLimitSwitch();
 
 void setup() {
   Serial.begin(115200);
+  dht.setup(PIN_DHT22, DHTesp::DHT22);
   pinMode(PIN_MOTOR_POWER, OUTPUT);
   pinMode(PIN_MIN_POSITION_SWITCH, INPUT_PULLUP);
   pinMode(PIN_MAX_POSITION_SWITCH, INPUT_PULLUP);
   attachInterrupt(0, interruptMinLimitSwitch, FALLING);
   attachInterrupt(1, interruptMaxLimitSwitch, FALLING);
+  stepper.setRpm(MAX_RPM);
   gotoZero();
   Serial.print("[SETUP] Complete.\n");
 }
 
 void loop() {
-  stepper.run();
-  if (stepper.getStepsLeft() == 0) {
-    Serial.print("[LOOP] Current turn: " + String(currentTurn) + "\n");
-    turnToPush(1);
-    delay(3000);
+  static float humidity = 0.0;
+  static float temperature = 0.0;
+
+  if (millis() > timerMillis + 1000) {
+    humidity = dht.getHumidity();
+    temperature = dht.getTemperature();
+    timerMillis = millis();
+    Serial.print("T: " + String(temperature) + " H: " + String(humidity) +
+                 "\n");
+    pushActuator();
   }
 }
 
 void interruptMinLimitSwitch() {
-  // powerOffStepper();
+  powerOffStepper();
   stepper.stop();
   currentTurn = 0;
 }
@@ -56,7 +66,6 @@ void interruptMinLimitSwitch() {
 void interruptMaxLimitSwitch() {
   powerOffStepper();
   stepper.stop();
-  maxTurn = currentTurn;
 }
 
 /* Установка актуатора в 0 положение.
@@ -64,7 +73,6 @@ void interruptMaxLimitSwitch() {
  * контроллера. */
 void gotoZero() {
   digitalWrite(PIN_MOTOR_POWER, HIGH);
-  stepper.setRpm(MAX_CCW_RPM);
   currentTurn = -1;
   Serial.print("[CALIBRATION] Start ");
   while (currentTurn) {
@@ -91,21 +99,23 @@ void powerOnStepper() {
 
 void powerOffStepper() { digitalWrite(PIN_MOTOR_POWER, LOW); }
 
-void turnToPush(uint16_t turns) {
-  if (digitalRead(PIN_MAX_POSITION_SWITCH) || currentTurn < MAX_TURN_LIMIT) {
+void pushActuator() {
+  uint16_t maxSwitchValue = digitalRead(PIN_MAX_POSITION_SWITCH);
+  if (maxSwitchValue && currentTurn < MAX_TURN_LIMIT) {
     powerOnStepper();
-    stepper.setRpm(MAX_CW_RPM);
+    stepper.moveDegreesCW(360);
     ++currentTurn;
-    maxTurn < currentTurn ? maxTurn = currentTurn : false;
-    stepper.newMoveDegreesCW(360 * turns);
+    Serial.print("Turn: " + String(currentTurn) + "\n");
   }
+  powerOffStepper();
 }
 
-void turnToPull(uint16_t turns) {
+void pullActuator() {
   if (digitalRead(PIN_MIN_POSITION_SWITCH)) {
     powerOnStepper();
-    stepper.setRpm(MAX_CCW_RPM);
+    stepper.moveDegreesCCW(360);
     --currentTurn;
-    stepper.newMoveDegreesCCW(360 * turns);
+    Serial.print("Turn: " + String(currentTurn) + "\n");
   }
-};
+  powerOffStepper();
+}
